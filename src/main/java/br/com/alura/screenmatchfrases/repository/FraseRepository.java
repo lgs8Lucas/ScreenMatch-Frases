88b6329aae5894b652058b87eee4e1fe3dbdfcd9
Lucas <lgs.08lucas@gmail.com>
2025-07-05 16:35:45 -0300

    Criado repositório e query para buscar uma frase aleatória no banco.

---
+++ src/main/java/br/com/alura/screenmatchfrases/repository/FraseRepository.java
@@ -0,0 +1,14 @@
+package br.com.alura.screenmatchfrases.repository;
+
+import br.com.alura.screenmatchfrases.models.Frase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface FraseRepository extends JpaRepository<Frase, Long> {
+
+    @Query("SELECT f FROM Frase f order by function('RANDOM') LIMIT 1")
+    List<Frase> obterFraseAlteatoria();
+
+}

da0031364578acafffce6963fa849a7a385686f9
Lucas <lgs.08lucas@gmail.com>
2025-07-05 16:47:57 -0300

    Corrigido repository

--- src/main/java/br/com/alura/screenmatchfrases/repository/FraseRepository.java
+++ src/main/java/br/com/alura/screenmatchfrases/repository/FraseRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FraseRepository extends JpaRepository<Frase, Long> {
 
     @Query("SELECT f FROM Frase f order by function('RANDOM') LIMIT 1")
-    List<Frase> obterFraseAlteatoria();
+    Optional<Frase> obterFraseAlteatoria();
 
 }
